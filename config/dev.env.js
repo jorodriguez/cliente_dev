c62ff7c2163d5d365a023ed4f3bfe4bda8c43687
joel <joel@localhost.localdomain>
2019-05-14 09:23:02 -0500

    Cambios en el archivo de parametros

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  ROOT_API : 'https://app-restexpres.herokuapp.com/',
-  API_LOGIN : 'https://app-restexpres.herokuapp.com/login'
+  ROOT_API : '"https://app-restexpres.herokuapp.com/"',
+  API_LOGIN : '"https://app-restexpres.herokuapp.com/login"'
   
 });
