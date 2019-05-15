145abe2b7484c7966519c49a323ac7c3d297e6c7
joel rodriguez <joel.rod.roj@hotmail.com>
2019-05-15 17:40:13 -0500

    cambios en la url del login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -38,7 +38,7 @@ export default {
         password: ""
       },
       response: "",
-      uriTemp:'https://app-restexpres.herokuapp.com/login'
+      uriTemp:'https://app-restexpres.herokuapp.com/auth/login'
       //uriTemp: "http://localhost:5000/auth/login"
     };
   },
