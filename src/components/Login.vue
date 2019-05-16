2a357b59dab76346d84250b542af639f062446c5
joel rodriguez <joel.rod.roj@hotmail.com>
2019-05-16 13:27:38 -0500

    Se agrego el logo

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login container">
-    <!--<img src="../assets/magic.jpeg" class="rounded-lg" width="150" height="100">-->    
+    <img src="../assets/magic.jpeg" class="rounded-lg" width="150" height="100">
     <form class="form-signin">
       <h1 class="h3 mb-3 font-weight-normal"></h1>
       <input
@@ -39,8 +39,8 @@ export default {
         password: ""
       },
       response: "",
-     // uriTemp:'https://app-restexpres.herokuapp.com/auth/login'
-      uriTemp: "http://localhost:5000/auth/login"
+      uriTemp:'https://app-restexpres.herokuapp.com/auth/login'
+     // uriTemp: "http://localhost:5000/auth/login"
     };
   },
   mounted() {
