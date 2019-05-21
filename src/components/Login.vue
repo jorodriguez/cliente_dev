2b8987dc84ef548a43140d9dbf96af4fe616e636
joel rodriguez <joel.rod.roj@hotmail.com>
2019-05-21 18:26:53 -0500

    cambios

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="login container">
-    <img src="../assets/magic.jpeg" class="rounded-lg" width="150" height="100"/>
-    
-    <form class="form-signin">
+      <img src="../assets/magic.jpeg" class="rounded-lg" width="150" height="100"/>
+        <form class="form-signin">
       <h1 class="h3 mb-3 font-weight-normal"></h1>
       <input
         type="email"
@@ -19,10 +18,36 @@
         placeholder="Password"
         required
       >
-      <button class="btn btn-lg btn-primary btn-block" v-on:click="login()">Entrar</button>
-      <br>
-      <br>
-      <p>{{ response }}</p>
+      <button class="btn btn-lg btn-primary btn-block"
+       v-on:click="login()">Entrar</button>    
+      <br/>
+      <br/>
+      <!--<p>{{ response }}</p>-->
+
+      <!-- MODAL TOAST -->
+      <div
+        id="toast_id"
+        role="alert"
+        aria-live="assertive"
+        aria-atomic="true"
+        class="toast border-warning"
+        data-autohide="true"
+        data-delay="1000"
+      >
+        <div class="toast-header p-1 mb-1 bg-warning text-dark">
+          <!--<img src="" class="rounded mr-2" alt="...">-->
+          <strong class="mr-auto">Login</strong>
+          <!--<small>11 mins ago</small>-->
+          <button type="button" class="ml-2 mb-1 close" 
+                data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="toast-body">
+          <p id="toast_msg"></p>
+        </div>
+      </div>
+      <!-- MODAL TOAST -->
     </form>
   </div>
 </template>
@@ -31,46 +56,59 @@
 //import axios from "axios";
 
 export default {
-
   name: "Login",
   data() {
     return {
       input: {
         correo: "",
-        password: ""
+        password: "",
+        mensajeToast:null
       },
       response: "",
-      uriTemp:'https://app-restexpres.herokuapp.com/auth/login'
+      uriTemp: "https://app-restexpres.herokuapp.com/auth/login"
       //uriTemp: "http://localhost:5000/auth/login"
     };
   },
   mounted() {
     console.log("iniciando login ");
     this.$session.clear();
-
+      //toast
+    this.mensajeToast = mensaje => {
+      $("#toast_msg").text(mensaje);
+      $("#toast_id").toast("show");
+    };
   },
   methods: {
     login() {
-      console.log("login con rest ");      
+      console.log("login con rest ");
+
+      if (!this.input.correo || !this.input.password) {
+        this.mensajeToast("Escribe tu usuario y tu clave.");
+        return;
+      }
+
       var data = { correo: this.input.correo, password: this.input.password };
-    
-      this.$http.post(this.uriTemp, data, { emulateJSON: true }).then(
-      //this.$http.post(process.env.API_LOGIN, data, { emulateJSON: true }).then(
+
+      this.$http.post(this.uriTemp, data, { emulateJSON: true }).then(        
         result => {
           console.log("En el login");
           this.response = result.data;
-
-          if (this.response.auth) {            
+          //this.mensajeToast(JSON.stringify(result));
+          if (this.response.auth) {
             this.$session.set("usuario_sesion", this.response);
-            this.$session.set("jwt", this.response.token);            
+            this.$session.set("jwt", this.response.token);
             this.$router.push({ name: "PaginaPrincipal" });
           } else {
+            this.mensajeToast("No se encuentra el usuario..");
             this.response = "No se encuentra el usuario..";
           }
         },
-        error => {
-          console.log(error);
-          this.response = error;
+        error => { //FIXME: modificar la contestacion del API
+          //console.log(JSON.stringify(error));
+          if(!error.auth){
+             this.mensajeToast("El usuario o la clave son incorrectas.");
+          }          
+          //this.response = error;
         }
       );
     }
--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,15 @@ import CatAlumno from '@/components/CatAlumno'
 import PerfilAlumno from '@/components/PerfilAlumno'
 import Asistencia from '@/asistencia/Asistencia'
 import RegistroActividad from '@/actividad/RegistroActividad'
+import Home from '@/components/Home'
 import VueSession from 'vue-session'
 
 Vue.use(Router);
 Vue.use(VueSession);
 
-//{ path: '/', name: 'Login', component: Login },
-export default new Router({
-  //mode: 'history',
-  routes: [
-    { path: '/', name:'Login', component: Login },
+export default new Router({      
+  routes: [    
+    { path: '/', name:'Login', component: Login },        
     { path: '/principal', name: 'PaginaPrincipal', component: Principal },
     { path: '/CatAlumno', name: 'CatAlumno', component: CatAlumno },
     { path: '/PerfilAlumno/:id', name: 'PerfilAlumno', component: PerfilAlumno },
