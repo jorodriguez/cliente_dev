c148fa5d7efb0f21556a138d908c59270a6f41f3
joel <joel@localhost.localdomain>
2019-07-03 09:57:57 -0500

    Cambios

--- src/controller/LoginController.js
+++ src/controller/LoginController.js
@@ -13,6 +13,7 @@ export default {
       },
       response: "",
       uriTemp: "http://localhost:5000/auth/login"
+    
       //uriTemp: "https://api-ambiente-desarrollo.herokuapp.com/auth/login"
       //uriTemp: "https://api-ambiente-produccion.herokuapp.com/auth/login"
     };
