bf1fd6fe9e03fd28ef98e961049dfd32fa2b3635
joel <joel@localhost.localdomain>
2019-07-08 09:07:19 -0500

    Cambios para subir

--- src/controller/AsistenciaController.js
+++ src/controller/AsistenciaController.js
@@ -8,7 +8,7 @@ export default {
     return {
       uriTemp: "http://localhost:5000/asistencia",
       //uriTemp:'https://api-ambiente-desarrollo.herokuapp.com/asistencia',
-      //uriTemp:'https://api-ambiente-produccion.herokuapp.com/asistencia',
+     // uriTemp:'https://api-ambiente-produccion.herokuapp.com/asistencia',
       usuarioSesion: {},
       sesion: {},
       item: AlumnoModel,
