4fcc3cc99a80ecc60f58b6670fc5702aab234ce7
joel.rod.roj@hotmail.com <joel.rod.roj@hotmail.com>
2019-12-18 22:53:34 -0600

    Cambios en la url

--- src/helpers/Urls.js
+++ src/helpers/Urls.js
@@ -1,9 +1,9 @@
 
-const BASE = process.env.URL_API || "http://localhost:5000" ;
+//const BASE = process.env.URL_API || "http://localhost:5000" ;
 //const BASE = process.env.URL_API || 'https://api-ambiente-desarrollo.herokuapp.com'
 //const BASE = process.env.URL_API || 'https://api-ambiente-produccion.herokuapp.com';
 
-//const BASE =  'https://api-ambiente-produccion.herokuapp.com';
+const BASE =  'https://api-ambiente-produccion.herokuapp.com';
 
 
 const ASISTENCIA_BASE =  BASE + '/asistencia';
