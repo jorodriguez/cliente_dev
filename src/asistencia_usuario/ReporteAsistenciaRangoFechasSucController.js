5ed4fbd1716719520825effbc19d06b78deed9e0
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2020-10-07 18:29:57 -0500

    Cambios

--- src/asistencia_usuario/ReporteAsistenciaRangoFechasSucController.js
+++ src/asistencia_usuario/ReporteAsistenciaRangoFechasSucController.js
@@ -4,104 +4,94 @@ import { operacionesApi } from "../helpers/OperacionesApi";
 import URL from "../helpers/Urls";
 import { VueGoodTable } from 'vue-good-table';
 import Datepicker from 'vuejs-datepicker';
-import COLUMNS_TABLE_ASISTENCIA_SUCURSAL  from "../helpers/DatatableConfigAsistenciasSucursal";
-import COLUMNS_TABLE_ASISTENCIA_USUARIO_DETALLE  from "../helpers/DatatableConfigAsistenciaUsuarioDetalle";
+import COLUMNS_TABLE_ASISTENCIA_SUCURSAL from "../helpers/DatatableConfigAsistenciasSucursal";
+import COLUMNS_TABLE_ASISTENCIA_USUARIO_DETALLE from "../helpers/DatatableConfigAsistenciaUsuarioDetalle";
 import Popup from '../controller/Popup'
 import TABLE_CONFIG from "../helpers/DatatableConfig";
 //import GraficaCalendarioAsistencia from '../componentes_generales/CalendarioAsistenciaComponente';
-import {getUsuarioSesion,token} from '../helpers/Sesion';
+import { getUsuarioSesion, token } from '../helpers/Sesion';
 
 export default {
   name: "reporte-asistencia-usuario-rango",
   components: {
-    Datepicker,VueGoodTable,Popup
+    Datepicker, VueGoodTable, Popup
   },
-  mixins:[operacionesApi],
+  mixins: [operacionesApi],
   data() {
-    return {      
+    return {
       usuarioSesion: {},
       //sesion: {}, 
-      fecha_inicio:Date,
-      fecha_fin:Date,
-      listaAsistenciaSucursal: [],    
-      listaAsistenciaUsuario:[],                               
-      usuario_seleccionado:null,
-      columnas:COLUMNS_TABLE_ASISTENCIA_SUCURSAL,
-      columnasUsuario:COLUMNS_TABLE_ASISTENCIA_USUARIO_DETALLE,
-      TABLE_CONFIG:TABLE_CONFIG,
+      fecha_inicio: Date,
+      fecha_fin: Date,
+      listaAsistenciaSucursal: [],
+      listaAsistenciaUsuario: [],
+      usuario_seleccionado: null,
+      columnas: COLUMNS_TABLE_ASISTENCIA_SUCURSAL,
+      columnasUsuario: COLUMNS_TABLE_ASISTENCIA_USUARIO_DETALLE,
+      TABLE_CONFIG: TABLE_CONFIG,
       mensaje: "",
-      loading:false,
-      loadFunctionAsistenciaSuc:null
+      loading: false
     };
   },
   mounted() {
-    console.log("iniciando el componente reporte de asistencia ");
-    /*this.sesion = this.$session.get("usuario_sesion");
-
-    if (!this.sesion || !this.sesion.usuario) {
-      console.log("No tiene sesion");      
-      this.mensaje = "Expiró su sesión";
-      return;
-    }
-    this.usuarioSesion = this.sesion.usuario;*/
-    this.usuarioSesion =getUsuarioSesion();
-    this.fecha_inicio = new Date();
-    this.fecha_fin = new Date();
-    this.TABLE_CONFIG.PAGINATION_OPTIONS.perPage = 50;
-
-    this.loadFunctionAsistenciaSuc = ()=> {      
-      console.log(" "+URL.ASISTENCIA_USUARIO_REPORTE_SUC_RANGO_FECHA);
-      this.loading = true;
-      this.get(
-        URL.ASISTENCIA_USUARIO_REPORTE_SUC_RANGO_FECHA + this.usuarioSesion.co_sucursal+"/"+this.fecha_inicio+"/"+this.fecha_fin,
-        
-        (result) => {          
-          console.log("Consulta " + result.data);
-          if (result.data != null) {
-            this.listaAsistenciaSucursal = result.data;                        
-          }
-          this.loading = false;
-        });    
-    };
-
-    this.loadFunctionAsistenciaUsuario = ()=> {      
-      console.log(" "+URL.ASISTENCIA_USUARIO_REPORTE_USUARIO_RANGO_FECHA);
-      this.loading = true;
-      this.get(
-        URL.ASISTENCIA_USUARIO_REPORTE_USUARIO_RANGO_FECHA + this.usuario_seleccionado.id+"/"+this.fecha_inicio+"/"+this.fecha_fin,
-        
-        (result) => {          
-          console.log("Consulta " + JSON.stringify(result.data));
-          if (result.data != null) {
-            this.listaAsistenciaUsuario = result.data;                        
-          }
-          this.loading = false;
-        });    
-    };
-
-    //this.loadFunctionAsistenciaSuc();
+    console.log("iniciando el componente reporte de asistencia ");   
+    this.usuarioSesion = getUsuarioSesion();
     
+    this.init();
   },
   methods: {
-    cambiarFechaInicio(){
-         this.$nextTick(() => {
-           console.log(this.fecha_inicio)           
-         });        
+    init() {
+      this.fecha_inicio = new Date();
+      this.fecha_fin = new Date();
+      this.TABLE_CONFIG.PAGINATION_OPTIONS.perPage = 50;
     },
-    cambiarFechaFin(){
+    cambiarFechaInicio() {
       this.$nextTick(() => {
-        console.log(this.fecha_fin)        
-      });        
+        console.log(this.fecha_inicio)
+      });
     },
-    cargarRegistros(){
+    cambiarFechaFin() {
+      this.$nextTick(() => {
+        console.log(this.fecha_fin)
+      });
+    },
+    cargarRegistros() {
       this.loadFunctionAsistenciaSuc();
     },
-    verDetalleUsuario(item){
-        //cargar detalle de las faltas por usuario 
-        this.usuario_seleccionado = item;
-        this.loadFunctionAsistenciaUsuario();
-        $("#popup_detalle_asistencia").modal("show");
-        
-    }   
-  }   
+    verDetalleUsuario(item) {
+      //cargar detalle de las faltas por usuario 
+      this.usuario_seleccionado = item;
+      this.loadFunctionAsistenciaUsuario();
+      $("#popup_detalle_asistencia").modal("show");
+
+    },
+    loadFunctionAsistenciaSuc() {
+      console.log(" " + URL.ASISTENCIA_USUARIO_REPORTE_SUC_RANGO_FECHA);
+      this.loading = true;
+      this.get(
+        URL.ASISTENCIA_USUARIO_REPORTE_SUC_RANGO_FECHA + this.usuarioSesion.co_sucursal + "/" + this.fecha_inicio + "/" + this.fecha_fin,
+
+        (result) => {
+          console.log("Consulta " + result.data);
+          if (result.data != null) {
+            this.listaAsistenciaSucursal = result.data;
+          }
+          this.loading = false;
+        });
+    },
+    loadFunctionAsistenciaUsuario() {
+      console.log(" " + URL.ASISTENCIA_USUARIO_REPORTE_USUARIO_RANGO_FECHA);
+      this.loading = true;
+      this.get(
+        URL.ASISTENCIA_USUARIO_REPORTE_USUARIO_RANGO_FECHA + this.usuario_seleccionado.id + "/" + this.fecha_inicio + "/" + this.fecha_fin,
+
+        (result) => {
+          console.log("Consulta " + JSON.stringify(result.data));
+          if (result.data != null) {
+            this.listaAsistenciaUsuario = result.data;
+          }
+          this.loading = false;
+        });
+    }
+  }
 };
--- src/helpers/DatatableConfigAsistenciasSucursal.js
+++ src/helpers/DatatableConfigAsistenciasSucursal.js
@@ -29,8 +29,8 @@ const COLUMNS_TABLE_ASISTENCIA_SUCURSAL =
         filterable: true,
       },
       {
-        label: 'Dias Activos',
-        field: 'dias_trabajo',                
+        label: 'Dias Laborables',
+        field: 'dias_laborables',                
         thClass: 'text-center',
         tdClass: 'text-center',
         filterable: false,
@@ -47,7 +47,33 @@ const COLUMNS_TABLE_ASISTENCIA_SUCURSAL =
         field: 'hora_salida',                        
         filterable: false,
         hidden:true
-      }         
+      },
+      {
+        label: 'Sueldo Mensual',
+        field: 'sueldo_base_mensual',                                
+        hidden:true
+      },
+      {
+        label: 'Sueldo (Quincenal)',
+        field: 'sueldo_base_quincenal',                                
+        hidden:false
+      },
+      {
+        label: 'Pago Quincena',
+        field: 'pago_sueldo_quincenal',                                
+        hidden:false
+      }, 
+      {
+        label: 'Pago Mensual',
+        field: 'pago_sueldo_mensual',                                
+        hidden:true
+      },
+      {
+        label: 'porcentaje_falta',
+        field: 'porcentaje_falta',                                
+        hidden:true
+      }
+
 ];
 
 
