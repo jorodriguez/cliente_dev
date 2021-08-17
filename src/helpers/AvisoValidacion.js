ddfc3948301843cbd74dc55a9f494ea645404f6a
joel.rod.roj@hotmail.com <joel.rod.roj@hotmail.com>
2021-08-16 21:24:03 -0500

    cambios

---
+++ src/helpers/AvisoValidacion.js
@@ -0,0 +1,37 @@
+
+
+import Vue from 'vue'
+import * as moment from 'moment';
+import { validarEmail } from './CorreoValidacion'
+
+export const validarDatosAviso = (aviso) => {
+  console.log("# VALIDACION DE AVISO #");
+  if (aviso == null || aviso == undefined) {
+    return false;
+  } else {
+
+    if (aviso.nombre == '') {
+      Vue.prototype.$notificacion.error('Escribe el nombre', 'El nombre es requerido.');
+      return false;
+    }
+
+    if (!aviso.titulo) {      
+        Vue.prototype.$notificacion.error('Titulo requerido', 'El titulo es requerido.');
+        return false;      
+    }
+
+    if (!aviso.para) {      
+        Vue.prototype.$notificacion.error('Selecciona para', 'Selecciona para quien será el aviso.');
+        return false;      
+    }
+
+    if (!aviso.aviso) {      
+      Vue.prototype.$notificacion.error('Aviso requerido', 'Tienes que escribir el contenido del aviso.');
+      return false;      
+  }
+   
+
+    return true;
+  }
+}
+
---
+++ src/models/AvisoModel.js
@@ -0,0 +1,42 @@
+
+export class AvisoModel {
+    constructor(usuario) {
+        this.id = null;
+        this.titulo = "";
+        this.para = "";
+        this.aviso = "";
+        this.etiquetas = "";
+        this.nota_interna ="";
+        this.etiquetas =  "";
+        this.genero = null;       
+    }
+
+}
+
+
+export class AvisoBuilder {
+    constructor() { }
+
+    setId(id) { this.id = id; return this; }
+    setTitulo(titulo) { this.titulo = tituli; return this; }
+    setPara(para) { this.para = para; return this; }
+    setAviso(aviso) { this.aviso = aviso; return this; }
+    setEtiqueta(etiqueta) { this.etiqueta = etiqueta; return this; }
+    setNotaInterna(notaInterna) { this.notaInterna = notaInterna; return this; }
+    setEtiquetas(etiquetas) { this.etiquetas = etiquetas; return this; }
+    setGenero(genero) { this.genero = genero; return this; }
+    clear() {
+        this.setId(0);
+        this.setTitulo("");
+        this.setPara("");
+        this.setAviso("");
+        this.setEtiqueta("");
+        this.setNotaInterna("");        
+        this.setGenero(null);        
+    }
+
+    build() {
+        return new AvisoModel(this);
+    }
+
+}
