206db114c7e05158e7f7b414e40110166248d705
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2021-11-03 14:12:49 -0600

    trabajando con la reactivacion de alumnos

--- src/components/ReactivarAlumnosEliminados.vue
+++ src/components/ReactivarAlumnosEliminados.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="reactivar_alumnos">
     <h1>Reacivar Alumnos</h1>
-    <small>{{usuarioSesion.nombre_sucursal}}</small>
+    <small>{{ usuarioSesion.nombre_sucursal }}</small>
     <div class="text-left">
       <router-link to="/CatAlumno" class="btn btn-secondary btn-lg">
         <i class="fas fa-arrow-circle-left text-gray"></i>
       </router-link>
-      
     </div>
     <br />
-    <!--<form>--> 
+    <!--<form>-->
 
     <div class="card">
       <div class="card-body">
@@ -24,105 +23,67 @@
             aria-describedby="basic-addon2"
           />
           <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button" v-on:click="buscarPorNombre()">
+            <button
+              class="btn btn-outline-secondary"
+              type="button"
+              v-on:click="buscarPorNombre()"
+            >
               <i class="fas fa-search"></i>
             </button>
           </div>
         </div>
-
-        <div class="table-responsive">
-          <table class="table">
-            <thead>
-              <th></th>
-              <th>Nombre</th>
-              <th class="hidden-xs">Apellidos</th>
-              <th>Grupo</th>
-              <th>Hora de Entrada</th>
-              <th>Hora de Salida</th>
-              <th></th>              
-            </thead>
-            <tr v-for="row in lista" :key="row.id">
-              <td class="text-right" style="padding-left:2px;padding-right:2px;">
-                <!--src="row.foto == null ? (row.sexo == 'Niño' ? './../assets/avatar_niño.jpg':'./../assets/avatar_niña.jpg'):row.foto"-->
-                <img
-                  :src="row.foto"
-                  width="50"
-                  height="50"
-                  v-on:click="subirFotoPerfil(row.id)"
-                  alt="..."
-                  title="Actualizar foto de perfil"
-                  class="rounded-circle"
-                  style="cursor:pointer"
-                />
-              </td>
-              <td class="text-left" style="padding-left:0px;">
-                <button
-                  type="button"
-                  class="btn btn-link"
-                  title="Ver el perfil del alumno"
-                  v-on:click="verPerfil(row)"
-                >
-                  {{ row.nombre }}
-                  <span v-if="row.mostrar_nombre_carino">({{row.nombre_carino}})</span>
-                  <span class="text-danger">{{row.motivo_eliminacion}}</span>
-                </button>
-              </td>
-              <td class="hidden-xs">{{ row.apellidos }}</td>
-              <td>
-                <span
-                  :style="row.color ? 'background-color:'+row.color:'' "
+      <div class="row " >
+      
+        <div class="col-3  " v-for="row in lista" :key="row.id">
+          <div class="card border border-secondary " >           
+            <img class="card-img-top rounded-circle"  :src="row.foto" alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">{{ row.nombre }}
+               <span v-if="row.mostrar_nombre_carino"
+                  >({{ row.nombre_carino }})</span>
+              </h5>              
+              <p class="card-text small">
+                {{ row.apellidos }}
+              </p>              
+              <p
+                  :style="row.color ? 'background-color:' + row.color : ''"
                   class="badge badge-info text-wrap"
-                >{{ row.nombre_grupo }}</span>
-              </td>
-              <td>{{ row.hora_entrada }}</td>
-              <td>{{ row.hora_salida }}</td>
-              <td>
-                <div class="btn-group" v-if="!row.adeuda">
-                  <button
-                    type="button"
-                    class="btn btn-link text-gray"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    <i class="fas fa-ellipsis-v"></i>
-                  </button>
-                  <div class="dropdown-menu bg-light">
-                    <button
-                      class="dropdown-item"
-                      v-on:click="cambiarSucursal(row)"
-                      title="Cambiar de sucursal."
-                    >
-                      <i class="fas fa-exchange-alt"></i>Cambiar de sucursal
-                    </button>
-                    <button
-                      class="dropdown-item text-danger"
-                      v-on:click="select(row,'DELETE')"
-                      title="Dar de baja el alumno."
-                    >
-                      <i class="fas fa-user-minus"></i> Iniciar Baja
-                    </button>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </table>
+                  >{{ row.nombre_grupo }}
+              </p>
+              <p>{{row.hora_entrada}}</p>
+              <p>{{row.hora_salida}}</p>
+            </div>
+            <!--<ul class="list-group list-group-flush">
+              <li class="list-group-item"> 
+              <span
+                  :style="row.color ? 'background-color:' + row.color : ''"
+                  class="badge badge-info text-wrap"
+                  >{{ row.nombre_grupo }}
+                </span></li>
+              <li class="list-group-item">{{row.hora_entrada}}</li>
+              <li class="list-group-item">{{row.hora_salida}}</li>
+            </ul>-->
+            <div class="card-footer">        
+              <button type="button" class="btn btn-outline-danger btn-block">Activar</button>    
+              <!--<a href="#" class="card-link text-danger">Reactivar</a>   -->
+            </div>
+          </div>
         </div>
+     
+      </div>
       </div>
     </div>
   </div>
 </template>
 
-<script >
+<script>
 import URL from "../helpers/Urls";
 import { operacionesApi } from "../helpers/OperacionesApi";
-import { getUsuarioSesion } from '../helpers/Sesion';
+import { getUsuarioSesion } from "../helpers/Sesion";
 
 export default {
   name: "cat_alumnos_eliminados",
-  components: {
-    
-  },
+  components: {},
   mixins: [operacionesApi],
   data() {
     return {
@@ -131,8 +92,7 @@ export default {
       criterioNombre: "",
       lista: [],
       listaRespaldo: [],
-      eliminados:true,
-    
+      eliminados: true
     };
   },
   mounted() {
@@ -140,39 +100,39 @@ export default {
 
     this.usuarioSesion = getUsuarioSesion();
     this.cargarAlumnosEliminados();
-  
   },
   methods: {
     verPerfil(rowSelect) {
       console.log("fila seleccionada " + rowSelect.nombre);
       this.$router.push({ name: "PerfilAlumno", params: { id: rowSelect.id } });
     },
-   async cargarAlumnosEliminados(){
-    this.lista = await this.getAsync(`${URL.ALUMNOS_BASE}/${this.usuarioSesion.co_sucursal}/eliminados/true`);
-    //this.listaRespaldo = this.response;
+    async cargarAlumnosEliminados() {
+      this.lista = await this.getAsync(
+        `${URL.ALUMNOS_BASE}/${this.usuarioSesion.co_sucursal}/eliminados/true`
+      );
+      //this.listaRespaldo = this.response;
     },
-    
+
     buscarPorNombre() {
       console.log("Buscar por nombre " + this.criterioNombre);
-      if (this.criterioNombre == '') {
+      if (this.criterioNombre == "") {
         this.lista = this.listaRespaldo;
       } else {
-
-        this.lista = this.listaRespaldo
-          .filter(
-            e =>
-              e.nombre.toUpperCase().includes(this.criterioNombre.toUpperCase())
-              || (e.nombre_carino ? e.nombre_carino.toUpperCase().includes(this.criterioNombre.toUpperCase()) : false)
-
-
-          );
-
+        this.lista = this.listaRespaldo.filter(
+          e =>
+            e.nombre
+              .toUpperCase()
+              .includes(this.criterioNombre.toUpperCase()) ||
+            (e.nombre_carino
+              ? e.nombre_carino
+                  .toUpperCase()
+                  .includes(this.criterioNombre.toUpperCase())
+              : false)
+        );
       }
-    },
-    
+    }
   }
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
