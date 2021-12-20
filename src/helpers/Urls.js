8bff560e0caa2562b7a6566473949716c0a1b9fd
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2021-12-20 14:02:09 -0600

    url a desa

--- src/helpers/Urls.js
+++ src/helpers/Urls.js
@@ -2,8 +2,8 @@ import configEnv from '../../config/configEnv';
 
 
 //const BASE = configEnv.BASE || "http://localhost:5000";
-//const BASE = process.env.URL_API || 'https://magic-stagging-api.herokuapp.com'
-const BASE = process.env.URL_API || 'https://api-ambiente-produccion.herokuapp.com';
+const BASE = process.env.URL_API || 'https://magic-stagging-api.herokuapp.com'
+//const BASE = process.env.URL_API || 'https://api-ambiente-produccion.herokuapp.com';
 
 export const BRANCH = "202110-hotfix-reactivar-alumno";
 
